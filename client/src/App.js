264e29548bf5c4e0b7e2248825324a7af7d8eb90
Vinayak Mishra <vinayakmishra26@gmail.com>
2022-08-16 17:15:02 +0530

    init

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './Container/Home/Home';
 import Calculate from './Container/Calculate/Calculate';
+import AboutUs from './Container/AboutUs/AboutUs';
 
 function App() {
   return (
     <div className="App">
       <Home/>
       <Calculate/>
+      <AboutUs/>
     </div>
   );
 }
