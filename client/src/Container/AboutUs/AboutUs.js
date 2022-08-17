3f015269a87ac6ed620a195d73d8c61c7472d9cb
Vinayak Mishra <vinayakmishra26@gmail.com>
2022-08-17 10:20:30 +0530

    init AboutUs

--- client/src/Container/AboutUs/AboutUs.js
+++ client/src/Container/AboutUs/AboutUs.js
@@ -1,20 +1,10 @@
 import React from "react";
 import "./AboutUs.css";
-import pic from "../../img/pic.jpg"
 
 export default function AboutUs() {
   return (
     <div>
-        <div className="about-us">
-          <h1 className="container">ABOUT US</h1>
-            <div className="aboutus-container">
-                <div className="aboutus-details">
-                    <div className="profile-container">
-                      <img className="pic" src={pic}></img>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
+    
   );
 }
