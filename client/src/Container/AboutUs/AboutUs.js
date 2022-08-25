eb5c4fdb4688c93ef2219b44e465ad18808b0339
Aarjav <aarjavjain889@gmail.com>
2022-08-25 09:03:19 +0530

    aboutus

--- client/src/Container/AboutUs/AboutUs.js
+++ client/src/Container/AboutUs/AboutUs.js
@@ -2,6 +2,12 @@ import React from 'react'
 import CardExampleCard from './card'
 import ScrollService from "../../utilities/ScrollService";
 import Animations from "../../utilities/Animations";
+import vansh from "./images/vansh.png"
+import aveesh from "./images/avneesh.png"
+import swapil from "./images/swapnil.png"
+import vinyak from "./images/vinyak.jpeg"
+import aditi from "./images/aditi.png"
+import aarjav from "./images/aarajvj.png"
 export default function AboutUs(props) 
 {
   let fadeInScreenHandler = (screen) => {
@@ -13,15 +19,22 @@ export default function AboutUs(props)
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
   return (
     <>
-    <div id={props.id || ""}>
-    <div style={{display:"flex", gap: "10px", marginBottom: "10rem"}}>
-    <CardExampleCard image={"./../vinyak.jpg"} />
-    <CardExampleCard image={"./..avesh.jpeg"} />
-    <CardExampleCard image={"././images/aarjav.jpg"}/>
-    </div><div style={{display:"flex", gap: "5px"}}>
-    <CardExampleCard image={"./../aarjav.png" } text=" aarjav jain"/>
-    <CardExampleCard image={"./../image.png"}/>
-    <CardExampleCard image={"./../image.png"}/>
+    <div id={props.id || ""} style={{background: "#FDF4DC",fontFamily: "Rubik-semibold", 
+      fontSize: "3rem",
+      padding: "6rem 2rem",
+      color: "#ffffff",
+      textAlign: "center",
+      width: "100%"}}>
+    
+    <h1>ABOUT US</h1>
+    <div style={{display:"flex", gap: "10px", marginBottom: "8rem"}}>
+    <CardExampleCard image={aditi}text=" Aditi Pundir" />
+    <CardExampleCard image={vansh} text=" Vansh Rana"/>
+    <CardExampleCard image={swapil} text=" Swapnil Sharma"/>
+    </div><div style={{display:"flex", gap: "10px"}}>
+    <CardExampleCard image={vinyak} text=" Vinayak Mishra"/>
+    <CardExampleCard image={aarjav} text=" Aarjav Jain"/>
+    <CardExampleCard image={aveesh}text=" Avneesh Sirohi"/>
     </div>
     </div>
   </>
--- client/src/Container/AboutUs/card.js
+++ client/src/Container/AboutUs/card.js
@@ -3,48 +3,52 @@ import React  from "react";
 const CardExampleCard = (props) => (
   <div
     style={{
-      background: "#fff",
+      background:"",
       display: "flex",
       flexDirection: "column",
       padding: "5px",
-      border: "1px solid #eee",
+      paddingLeft: "80px",
+      border: "1px  #eee",
       width: "500px",
       overflow:"hidden"
     }}
   >
     <img
       src={props.image}
-      width="100%"
-      height="230px"
+      width="75%"
+      height="250px"
       alt=""
     />
-    <div style={{border:"0.5px solid #eee", marginTop:"5px"}}>
+  <div style={{border:"0.5px #eee", marginTop:"5px"}}>
       <div
         style={{
           textAlign: "center",
           fontSize: "14px",
-          fontWeight: "600"
+          fontWeight: "600",
+          color:"black"
         }}
       >
         {props.text}
         
         
       </div>
-      <div
+     {/* <div
         style={{
           fontSize: "12px",
           fontWeight: "600",
           marginTop: "10px",
-          textAlign:"center"
+          textAlign:"center",
+          color:"white"
         }}
       >
         Random Details on the image or the Team Member<br/>
         Random Details on the image or the Team Member<br/>
         Random Details on the image or the Team Member<br/>
         Random Details on the image or the Team Member<br/>
-      </div>
+      </div>*/}
     </div>
   </div>
+  
 );
 
 export default CardExampleCard;
